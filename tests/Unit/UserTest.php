69205f88bdff9e4e806fb52d6485b020612ce9d6
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-03 09:44:04 +1000

    Create UserTest.php
    Added test to open login form page.
    Added test to check if users were duplicated.
    Added test to delete user.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User as User;
+use Tests\TestCase;
+
+
+class UserTest extends TestCase
+{
+    /**
+     *
+     * @return void
+     */
+    public function test_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_duplication()
+    {
+        $user1 = User::make([
+            'name'=> 'Jane Doe',
+            'email'=> 'janedoe@email.com'
+        ]);
+        $user2 = User::make([
+            'name'=> 'Jane Rue',
+            'email'=> 'janerue@email.com'
+        ]);
+
+        $this->assertTrue($user1->name != $user2->name);
+    }
+
+    public function test_delete_user()
+    {
+        $user = User::factory()->count(1)->make();
+
+        $user = User::first();
+
+        if($user) {
+            $user->delete();
+        }
+
+        $this->assertTrue(true);
+    }
+}
